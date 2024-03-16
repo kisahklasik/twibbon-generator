a9a6df3df9a719bc6a735613be949db1db548c07
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2024-03-16 14:09:37 +0700

    Update share.js

--- src/js/share.js
+++ src/js/share.js
@@ -1,21 +1,21 @@
 let shareData = {
-        title: 'BERA APP',
-        text: 'Bera App',
-        url: 'https://bera.pages.dev/',
-      }
+title: 'BERA APP',
+text: 'Bera App',
+url: 'https://bera.pages.dev/',
+}
 
-      const btn = document.querySelector('em');
-      const resultPara = document.querySelector('.result');
+const btn = document.querySelector('em');
+const resultPara = document.querySelector('.result');
 
-      btn.addEventListener('click', () => {
-        navigator.share(shareData)
-          .then(() =>
-            resultPara.textContent = 'Twibbon shared successfully'
-          )
-          .catch((e) =>
-            resultPara.textContent = 'Error: ' + e
-          )
-      });
+btn.addEventListener('click', () => {
+navigator.share(shareData)
+.then(() =>
+resultPara.textContent = 'Twibbon shared successfully'
+)
+.catch((e) =>
+resultPara.textContent = 'Error: ' + e
+)
+});
 
 // copy
 bera("", "rgba(255,255,255,.5)");
