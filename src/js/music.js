00f948cac98d83434abf2826b13e193a5a4503d4
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2024-03-26 04:40:44 +0700

    Update music.js

--- src/js/music.js
+++ src/js/music.js
@@ -1,23 +1,5 @@
-let audiElem = document.getElementById("audio");
-let playButton = document.getElementById("playbutton");
+// get the audio element
+const audioElement = document.querySelector("audio");
 
-playButton.addEventListener("click", handlePlayButton, false);
-playAudio();
-
-async function playAudio() {
-  try {
-    await audioElem.play();
-    playButton.className = "playing";
-  } catch(err) {
-    playButton.className = "";
-  }
-}
-
-function handlePlayButton() {
-	if (audioElem.paused) {
-  	playAudio();
-  } else {
-  	audioElem.pause();
-    playButton.className = "";
-  }
-}
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
