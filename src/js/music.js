42a7d082e55ab63a705440d239b94e1945da8d46
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2024-03-26 09:41:52 +0700

    Update music.js

--- src/js/music.js
+++ src/js/music.js
@@ -1,5 +1,17 @@
-// get the audio element
-const audioElement = document.querySelector("audio");
+window.onload = () => {
+  const audio = document.getElementById("my-audio");
+  const play = document.getElementById("play");
+  const pause = document.getElementById("pause");
 
-// pass it into the audio context
-const track = audioContext.createMediaElementSource(audioElement);
+  // associate functions with the 'onclick' events
+  play.onclick = playAudio;
+  pause.onclick = pauseAudio;
+
+  function playAudio() {
+    audio.play();
+  }
+
+  function pauseAudio() {
+    audio.pause();
+  }
+};
