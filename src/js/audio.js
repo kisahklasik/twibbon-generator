341c33f83fa6e4afecec4aa8a1c901243f349e31
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2024-03-26 18:26:41 +0700

    Create audio.js

---
+++ src/js/audio.js
@@ -0,0 +1,33 @@
+const url = "https://upload.wikimedia.org/wikipedia/en/a/a9/Webern_-_Sehr_langsam.ogg";
+const audio = new Audio(url);
+const playBtn = document.querySelector("button");
+const progressEl = document.querySelector('input[type="range"]');
+let mouseDownOnSlider = false;
+
+audio.addEventListener("loadeddata", () => {
+  progressEl.value = 0;
+});
+audio.addEventListener("timeupdate", () => {
+  if (!mouseDownOnSlider) {
+    progressEl.value = audio.currentTime / audio.duration * 100;
+  }
+});
+audio.addEventListener("ended", () => {
+  playBtn.textContent = "▶️";
+});
+
+playBtn.addEventListener("click", () => {
+  audio.paused ? audio.play() : audio.pause();
+  playBtn.textContent = audio.paused ? "▶️" : "⏸️";
+});
+
+progressEl.addEventListener("change", () => {
+  const pct = progressEl.value / 100;
+  audio.currentTime = (audio.duration || 0) * pct;
+});
+progressEl.addEventListener("mousedown", () => {
+  mouseDownOnSlider = true;
+});
+progressEl.addEventListener("mouseup", () => {
+  mouseDownOnSlider = false;
+});
